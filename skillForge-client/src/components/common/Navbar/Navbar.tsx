48ac06396a83ecad64e4a49a26c93b75754b9b5c
RAHUL <rahulph719@gmail.com>
2026-02-19 11:33:35 +0530

    front end ui changes

--- skillForge-client/src/components/common/Navbar/Navbar.tsx
+++ skillForge-client/src/components/common/Navbar/Navbar.tsx
@@ -190,7 +190,7 @@ export default function Navbar() {
                             </span>
                         )}
                     </Link>
-                    <MessageCircle className="w-5 h-5 text-gray-600 cursor-pointer hover:text-blue-600 transition-colors" />
+                    {/* <MessageCircle className="w-5 h-5 text-gray-600 cursor-pointer hover:text-blue-600 transition-colors" /> */}
 
                     {/* User Menu Dropdown */}
                     <div className="relative pl-2 border-l border-gray-200">
